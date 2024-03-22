f458327ec006a0b934505c7c15427a1f04c00703
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-21 23:15:20 -0500

    Add html structure in login and register page.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { MaterialModule } from '../material/material.module';
 
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
@@ -15,7 +16,8 @@ import { RegisterPageComponent } from './pages/register-page/register-page.compo
     RegisterPageComponent
   ],
   imports: [
-    AuthRoutingModule
+    AuthRoutingModule,
+    MaterialModule
   ]
 })
 export class AuthModule { }
