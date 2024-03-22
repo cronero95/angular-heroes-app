068f7649d8edce6125831035a47ce374960ef9ad
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-21 20:34:02 -0500

    Prepare project structure for future work.

---
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'heroes-new-hero-page',
+  templateUrl: './new-hero-page.component.html',
+  styles: ``
+})
+export class NewHeroPageComponent {
+
+}
