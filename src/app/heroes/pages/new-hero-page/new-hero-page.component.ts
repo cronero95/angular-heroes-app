eacc0fcc5902419897b30cf5c072f9013e657efb
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-03-23 20:32:15 -0500

    Prepare 'new-hero' component structure.

--- src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
+++ src/app/heroes/pages/new-hero-page/new-hero-page.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class NewHeroPageComponent {
 
+  public publishers = [
+    {id: 'DC Comics', desc: 'DC Comics'},
+    {id: 'Marvel Comics', desc: 'Marvel Comics'}
+  ]
+
 }
